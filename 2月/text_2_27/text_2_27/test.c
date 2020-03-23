de851734f3e3ea775946cf1aceb03cb7211ed666
Straw-b <1609332043@qq.com>
2020-03-23 20:40:47 +0800

    提交代码

---
+++ 2月/text_2_27/text_2_27/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("hello\n");
+//	return 0;
+//}
+
+int main()
+{
+	printf("")
+	return 0;
+}
---
+++ test_3_22/test_3_22/test1.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+int* test()
+{
+	int a = 10;
+	return &a;
+}
+
+int main()
+{
+	int *p = test();
+	*p = 20;
+
+	return 0;
+}
