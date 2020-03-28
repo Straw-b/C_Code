ccd10c62191c4a53ce274f29b9870abacbbcdabe
Straw-b <1609332043@qq.com>
2020-03-28 12:33:10 +0800

    提交代码

---
+++ test_3_15/test_3_15/test2.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+////µÝ¹é
+//#include <stdio.h>
+//
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	char arr[] = "hello world";
+//	int len = my_strlen(arr);
+//	printf("len = %d\n", len);
+//	return 0;
+//}
+
+
+
+
+////·ÇµÝ¹é
+//#include <stdio.h>
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char arr[] = "hello world";
+//	int len = my_strlen(arr);
+//	printf("len = %d\n", len);
+//	return 0;
+//}
---
+++ test_3_28/test_3_28/test1.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+typedef struct Stu
+{
+	char name[20];
+	short age;
+	char tele[12];
+	char sex[5];
+}Stu;
+
+int main()
+{
+	Stu s1 = { "ÕÅÈý", 20, "12345678910", "ÄÐ" };
+	Stu s2 = { "Íú×Ð", 30, "12345678910", "±£ÃÜ" };
+	1
+
+	return 0;
+}
