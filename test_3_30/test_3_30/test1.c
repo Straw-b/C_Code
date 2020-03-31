99200eb8964bb67b83bea1b0350a4d00a6984397
Straw-b <1609332043@qq.com>
2020-03-31 16:38:46 +0800

    提交代码

---
+++ test_3_30/test_3_30/test1.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	return 0;
+}
