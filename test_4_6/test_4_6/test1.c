b892c2dc14454cd3205acc72c9204d2ac50d4f12
Straw-b <1609332043@qq.com>
2020-04-06 21:57:06 +0800

    提交代码

---
+++ test_4_6/test_4_6/test1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	char ch = 'w';
+	char* p = &ch;
+	const char* p2 = "abcdef";
+	int* arr[10];
+	char* ch[5];
+	return 0;
+}
