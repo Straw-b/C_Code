e19989491e78ab940442a422215c87f41b902a4b
Straw-b <1609332043@qq.com>
2020-04-08 21:55:05 +0800

    提交代码

---
+++ test_4_8/test_4_8/test1.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+void bubble_sort(void* base, int sz, int width, int(*cmp)(void*e1, void*e2))
+{
+	int i = 0;
+	//趟数
+	for (i = 0; i < sz - 1; i++)
+	{
+		//每一趟比较的对数
+		int j = 0;
+		for (j = 0; j < sz - 1; j++)
+		{
+			//两个元素的比较
+			if ()
+			{
+				//交换
+			}
+		}
+	}
+}
+
+int main()
+{
+
+	return 0;
+}
