67890363192413ac563fe3d262ad20fa3a5bef07
Straw-b <1609332043@qq.com>
2020-04-12 21:14:03 +0800

    提交代码

--- test_4_2/test_4_2/test.c
+++ test_4_2/test_4_2/test.c
@@ -37,7 +37,8 @@
 //	printf("*pFloat的值为：%f\n", *pFloat);//9.0
 //	return 0;
 //}
-//
+
+
 //int main()
 //{
 //	float f = 5.5;
@@ -263,39 +264,39 @@ void print1(int arr[3][5], int x, int y)
 	}
 }
 
-//参数是指针的形式
-void print2(int(*p)[5], int x, int y)
-{
-	int i = 0;
-	for (i = 0; i < x; i++)
-	{
-		int j = 0;
-		for (j = 0; j < y; j++)
-		{
-			printf("%d ", p[i][j]);
-			//printf("%d ", *(p[i] + j));
-			//printf("%d ", *(*(p + i) + j));
-			//printf("%d ", (*(p + i))[j]);
-		}
-		printf("\n");
-	}
-}
-
-int main()
-{
-	int arr[3][5] = { { 1, 2, 3, 4, 5 }, { 2, 3, 4, 5, 6 }, { 3, 4, 5, 6, 7 } };
-
-	//print1(arr, 3, 5);//arr - 数组名 - 数组名就是首元素地址
-	print2(arr, 3, 5);//
-	//int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-	//int i = 0;
-	//int* p = arr;
-	//for (i = 0; i < 10; i++)
-	//{
-	//	printf("%d ", p[i]);
-	//	printf("%d ", *(p + i));
-	//	printf("%d ", *(arr + i));
-	//	printf("%d ", arr[i]);//arr[i] == *(arr+i) == *(p+i) == p[i]
-	//}
-	return 0;
-}
+////参数是指针的形式
+//void print2(int(*p)[5], int x, int y)
+//{
+//	int i = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", p[i][j]);
+//			//printf("%d ", *(p[i] + j));
+//			//printf("%d ", *(*(p + i) + j));
+//			//printf("%d ", (*(p + i))[j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[3][5] = { { 1, 2, 3, 4, 5 }, { 2, 3, 4, 5, 6 }, { 3, 4, 5, 6, 7 } };
+//
+//	//print1(arr, 3, 5);//arr - 数组名 - 数组名就是首元素地址
+//	print2(arr, 3, 5);//
+//	//int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	//int i = 0;
+//	//int* p = arr;
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("%d ", p[i]);
+//	//	printf("%d ", *(p + i));
+//	//	printf("%d ", *(arr + i));
+//	//	printf("%d ", arr[i]);//arr[i] == *(arr+i) == *(p+i) == p[i]
+//	//}
+//	return 0;
+//}
--- test_4_2/test_4_2/test1.c
+++ test_4_2/test_4_2/test1.c
@@ -1,1 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 9;
+	//0 00000000 00000000000000000001001-补码
+	//
+	float *pFloat = (float *)&n;
+	printf("n的值为：%d\n", n);//9
+	printf("*pFloat的值为：%f\n", *pFloat);//0.000000
+	//(-1)^0 * 0.00000000000000000001001 * 2^-126
+	//
+	*pFloat = 9.0;
+	//1001.0
+	//1.001*2^3
+	//(-1)^0 *1.001 * 2^3
+	//01000001000100000000000000000000
+
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);//9.0
+	return 0;
+}
