fe8953564bc950f24075c176fa907d1a64f5bdd6
Straw <61579269+Straw-b@users.noreply.github.com>
2021-08-03 11:07:47 +0800

    Delete test_3_15 directory

--- test_3_15/test_3_15/add.c
+++
@@ -1,7 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-//函数的定义
-int Add(int x, int y)
-{
-	return x + y;
-}
--- test_3_15/test_3_15/add.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ADD_H__
-#define __ADD_H__
-
-//º¯ÊıµÄÉùÃ÷
-int Add(int x, int y);
-
-#endif
