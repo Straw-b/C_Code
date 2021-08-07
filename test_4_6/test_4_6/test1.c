86c3aea26480c501c0a383764b41e724287ad90c
Straw <61579269+Straw-b@users.noreply.github.com>
2021-08-07 16:21:14 +0800

    指针进阶2

---
+++ test_4_6/test_4_6/test1.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//void print(char *str)
+//{
+//	printf("hehe:%s", str);
+//}
+//
+//void test( void (*p)(char*) )
+//{
+//	printf("test\n");
+//	p("bit");
+//}
+//
+//int main()
+//{
+//	test(print);
+//	return 0;
+//}
