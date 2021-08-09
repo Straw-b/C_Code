50f9185895ce038fe27dbc2b5287e2f41a4d532d
Straw <61579269+Straw-b@users.noreply.github.com>
2021-08-09 09:20:33 +0800

    指针进阶4

--- test_4_10/test_4_10/test1.c
+++ test_4_10/test_4_10/test1.c
@@ -110,48 +110,48 @@
 //要求：时间复杂度小于O(N);
 #include <stdio.h>
 
-int FindNum(int arr[3][3], int k, int *px, int*py)
-{
-	int x = 0;
-	int y = *py - 1;
-
-	while (x <= *px - 1 && y >= 0)
-	{
-		if (arr[x][y] > k)
-		{
-			y--;
-		}
-		else if (arr[x][y] < k)
-		{
-			x++;
-		}
-		else
-		{
-			*px = x;
-			*py = y;
-			return 1;
-		}
-	}
-	//找不到
-	return 0;
-}
-
-int main()
-{
-	int arr[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-	int k = 9;
-	int x = 3;
-	int y = 3;
-	//返回型参数
-	int ret = FindNum(arr, k, &x, &y);
-	if (ret == 1)
-	{
-		printf("找到了\n");
-		printf("下标是：%d %d\n", x, y);
-	}
-	else
-	{
-		printf("找不到\n");
-	}
-	return 0;
-}
+//int FindNum(int arr[3][3], int k, int *px, int*py)
+//{
+//	int x = 0;
+//	int y = *py - 1;
+//
+//	while (x <= *px - 1 && y >= 0)
+//	{
+//		if (arr[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			*px = x;
+//			*py = y;
+//			return 1;
+//		}
+//	}
+//	//找不到
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+//	int k = 9;
+//	int x = 3;
+//	int y = 3;
+//	//返回型参数
+//	int ret = FindNum(arr, k, &x, &y);
+//	if (ret == 1)
+//	{
+//		printf("找到了\n");
+//		printf("下标是：%d %d\n", x, y);
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
