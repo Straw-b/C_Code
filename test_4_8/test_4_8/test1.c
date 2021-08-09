9f47e51d3aeb154cde33ed2fa9e340abc05b7444
Straw <61579269+Straw-b@users.noreply.github.com>
2021-08-09 09:09:24 +0800

    指针进阶3

---
+++ test_4_8/test_4_8/test1.c
@@ -0,0 +1,172 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+//void Swap(char* buf1, char* buf2, int width)
+//{
+//	int i = 0;
+//	for (i = 0; i < width; i++)
+//	{
+//		char tmp = *buf1;
+//		*buf1 = *buf2;
+//		*buf2 = tmp;
+//		buf1++;
+//		buf2++;
+//	}
+//}
+//
+//void bubble_sort(void* base, int sz, int width, int(*cmp)(void* e1, void* e2))
+//{
+//	int i = 0;
+//	//趟数
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//每一趟比较的对数
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			//两个元素的比较
+//			if (cmp((char*)base + j*width, (char*)base + (j + 1)*width) > 0)
+//			{
+//				//交换
+//				Swap((char*)base + j*width, (char*)base + (j + 1)*width, width);
+//			}
+//		}
+//	}
+//}
+//
+//
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int cmp_stu_by_age(const void* e1, const void* e2)
+//{
+//	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+//
+//int cmp_stu_by_name(const void* e1, const void* e2)
+//{
+//	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+//}
+//
+//int main()
+//{
+//	struct Stu s[3] = { { "zhangsan", 20 }, { "lisi", 30 }, { "wangwu", 10 } };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	//bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+//	bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+//	return 0;
+//}
+
+
+
+
+////模仿qsort的功能实现一个通用的冒泡排序
+//#include <stdio.h>
+//
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+//
+//void Swap(char* buf1, char* buf2, int width)
+//{
+//	int i = 0;
+//	for (i = 0; i < width; i++)
+//	{
+//		char tmp = *buf1;
+//		*buf1 = *buf2;
+//		*buf2 = tmp;
+//		buf1++;
+//		buf2++;
+//	}
+//}
+//
+//void bubble_sort(void* base, int sz, int width, int (*cmp)(void* e1, void* e2))
+//{
+//	int i = 0;
+//	//趟数
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//每一趟比较的对数
+//		int j = 0;
+//		for (j = 0; j < sz-1-i; j++)
+//		{
+//			//两个元素的比较
+//			if (cmp( (char*)base+j*width, (char*)base+(j+1)*width ) > 0)
+//			{
+//				//交换
+//				Swap( (char*)base + j*width, (char*)base + (j + 1)*width, width );
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+
+//练习使用库函数，qsort排序各种类型的数据
+//#include <stdio.h>
+//
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//
+//int cmp_float(const void* e1, const void* e2)
+//{
+//	if (*(float*)e1 == *(float*)e2)
+//		return 0;
+//	else if (*(float*)e1 > *(float*)e2)
+//		return 1;
+//	else
+//		return -1;
+//}
+//
+//int main()
+//{
+//	float f[] = { 9.0, 8.0, 7.0, 6.0, 5.0, 4.0 };
+//	int sz = sizeof(f) / sizeof(f[0]);
+//	qsort(f, sz, sizeof(f[0]), cmp_float);
+//	int j = 0;
+//	for (j = 0; j < sz; j++)
+//	{
+//		printf("%f ", f[j]);
+//	}
+//	return 0;
+//}
